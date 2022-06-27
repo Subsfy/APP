b4fc94d94fff6b328645883ed2424e723a72400e
Nicolas Maso <nicolas.masopb@hotmail.com>
2022-06-27 20:08:38 -0300

    Adjust on splash loading #18 | Update Redux config #19

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import AppLoading from 'expo-app-loading';
-import React from 'react';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Inter_500Medium } from '@expo-google-fonts/inter';
-import { Provider } from 'react-redux';
-import { Routes } from './src/routes';
-import { store } from './src/redux/store';
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import { NativeBaseProvider } from "native-base";
+import { Inter_500Medium } from "@expo-google-fonts/inter";
+import { Provider } from "react-redux";
+import { Routes } from "./src/routes";
+import { store } from "./src/redux/store";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  const [fontsLoaded] = useFonts({
-    Inter_500Medium,
-  });
+  useEffect(() => {
+    (async () => {
+      await SplashScreen.preventAutoHideAsync();
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+      await Font.loadAsync({
+        Inter_500Medium,
+      });
+
+      setAppIsReady(true);
+    })();
+  }, []);
+
+  useLayoutEffect(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
